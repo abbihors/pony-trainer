fcf8dcca12bdb4ba8d5f37114f9a6b6c66804106
abbiwyn <abbiwyn@gmail.com>
2021-04-02 11:29:59 +0300

    match librosa stft

--- src/spectrogram2.js
+++ src/spectrogram2.js
@@ -10,9 +10,9 @@ for (let i = 0; i < sr; i++) {
     samples[i] = Math.random() * 2 - 1;
 }
 
-let t0 = Date.now();
-let res = fft(samples);
-console.log(`took: ${Date.now() - t0} ms`);
+// let t0 = Date.now();
+// let res = fft(samples);
+// console.log(`took: ${Date.now() - t0} ms`);
 
 // console.log(res);
 
@@ -26,9 +26,17 @@ function fft(y) {
 // loadBuffer(fileUrl).then(audioBuffer => {
 loadExampleBuffer().then(audioBuffer => {
     let samples = audioBuffer.getChannelData(0);
-    console.log(samples.slice(0, 10));
-    let fft_data = fft(samples);
-    console.log(fft_data);
+    // console.log(samples.slice(0, 10));
+    // let fft_data = fft(samples);
+    // console.log(fft_data);
+
+    // match: librosa.stft(s, 2048, 2048//4, 2048, center=False).T
+    let t0 = Date.now();        
+    let res = stft(samples, 2048, 512);
+    console.log(`took: ${Date.now() - t0} ms`);
+
+    console.log(res);
+
 });
 
 const loadEl = document.querySelector('#load');
@@ -73,4 +81,65 @@ async function loadBufferOffline(url) {
     const offlineCtx = new OfflineAudioContext(1, 16000, 16000);
     return fetch(url).then(body => body.arrayBuffer())
         .then(buffer => offlineCtx.decodeAudioData(buffer));
+}
+
+function stft(y, fftSize = 2048, hopSize = fftSize / 4) {
+    // Split the input buffer into sub-buffers of size fftSize.
+    const bufferCount = Math.floor((y.length - fftSize) / hopSize) + 1;
+    let matrix = range(bufferCount).map(x => new Float32Array(fftSize));
+    for (let i = 0; i < bufferCount; i++) {
+        const ind = i * hopSize;
+        const buffer = y.slice(ind, ind + fftSize);
+        // In the end, we will likely have an incomplete buffer, which we should
+        // just ignore.
+        if (buffer.length != fftSize) {
+            continue;
+        }
+
+        const win = hannWindow(buffer.length);
+        const winBuffer = applyWindow(buffer, win);
+        const fft_res = fft(winBuffer);
+        // TODO: Understand why fft output is 2 larger than expected (eg. 1026
+        // rather than 1024).
+        matrix[i].set(fft_res.slice(0, fftSize));
+    }
+    return matrix;
+}
+
+
+function sum(array) {
+    return array.reduce(function (a, b) { return a + b; });
+}
+
+function range(count) {
+    let out = [];
+    for (let i = 0; i < count; i++) {
+        out.push(i);
+    }
+    return out;
+}
+
+function hannWindow(length) {
+    let win = new Float32Array(length);
+    for (let i = 0; i < length; i++) {
+        win[i] = 0.5 * (1 - Math.cos(2 * Math.PI * i / (length - 1)));
+    }
+    return win;
+}
+
+/**
+ * Applies a window to a buffer (point-wise multiplication).
+ */
+function applyWindow(buffer, win) {
+    if (buffer.length != win.length) {
+        console.error(`Buffer length ${buffer.length} != window length
+        ${win.length}.`);
+        return;
+    }
+
+    let out = new Float32Array(buffer.length);
+    for (let i = 0; i < buffer.length; i++) {
+        out[i] = win[i] * buffer[i];
+    }
+    return out;
 }
