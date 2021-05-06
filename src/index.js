2a5fde0b36022cdbc1f67d7c6c5d8f0462d8aced
abbiwyn <abbiwyn@gmail.com>
2021-05-06 09:05:33 +0300

    Add deviceAdded callback

--- src/index.js
+++ src/index.js
@@ -12,12 +12,35 @@ listenButton.onclick = () => {
     listenButton.style.display = 'none';
 }
 
+const slider = document.querySelector('#threshold');
+
+slider.oninput = () => {
+    let recordVol = document.querySelector('.voicemeter-recordvol');
+
+    recordVol.style.transform = 'translateX(80px)';
+}
+
 const connectButton = document.querySelector('#btn-connect');
 
 connectButton.onclick = () => {
     const results = document.querySelector('.wrapper-results');
 
     results.style.display = 'inline';
+
+    // FIND TOYS
+}
+
+function deviceAdded(deviceName) {
+    const results = document.querySelector('.wrapper-results');
+    let deviceLine = document.createElement('p');
+    let testButton = document.createElement('button');
+
+    deviceLine.innerText = `▸ ${deviceName}`;
+    testButton.className = 'pt-button-test';
+    testButton.innerText = 'Test vibration';
+
+    results.append(deviceLine);
+    results.append(testButton);
 }
 
 // let recordVolume = 0.04;
