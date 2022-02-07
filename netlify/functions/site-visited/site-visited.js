83fe26ac63e84d54907a52fef804dad9e5fe4fc0
abbiwyn <abbiwyn@gmail.com>
2022-02-07 13:46:49 +0200

    Ditch stupid analytics

--- netlify/functions/site-visited/site-visited.js
+++
@@ -1,9 +0,0 @@
-const handler = async (event) => {
-    console.log(`Got visitor at: ${new Date().toLocaleString()}`);
-
-    return {
-        statusCode: 200
-    }
-}
-
-module.exports = { handler }
